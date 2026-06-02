8af5d9f5f755472922c05166164cc4b0accbb47d
Sébastien Délèze <sebastien.deleze@iomedia.ch>
2026-06-02 16:27:08 +0200

    feat: refresh website

--- assets/app.js
+++ assets/app.js
@@ -1,14 +1,1 @@
-import "./bootstrap.js";
 import "./styles/app.css";
-
-addEventListener("turbo:frame-render", (event) => {
-  if (event.detail.fetchResponse.location.pathname === "/") {
-    document
-      .getElementsByClassName("modal-container")[0]
-      .classList.add("modal-hidden");
-  } else {
-    document
-      .getElementsByClassName("modal-container")[0]
-      .classList.remove("modal-hidden");
-  }
-});
